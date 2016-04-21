b23dbe5a587f6de225e4ede066a586593cd6bda4
Thomas Bärwinkel <tbl@gmx.de>
2016-04-21 11:40:55 +0200

    add the possibility to init data in a bean shell filter

--- src/main/java/de/tblsoft/solr/pipeline/filter/BeanShellFilter.java
+++ src/main/java/de/tblsoft/solr/pipeline/filter/BeanShellFilter.java
@@ -2,9 +2,16 @@ package de.tblsoft.solr.pipeline.filter;
 
 import bsh.EvalError;
 import bsh.Interpreter;
+import com.google.common.base.Strings;
 import de.tblsoft.solr.pipeline.AbstractFilter;
+import de.tblsoft.solr.util.IOUtils;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by tblsoft 20.04.16.
@@ -15,15 +22,33 @@ public class BeanShellFilter extends AbstractFilter {
 
     private String filename;
 
+    private Map<String, Object> init = new HashMap<String, Object>();
+
+    List<String> foooo = new ArrayList<String>();
+
     @Override
     public void init() {
-        filename = getProperty("filename", null);
-        verify(filename, "For the BeanShellFilter a filename property must be defined.");
+        String internalFilename = getProperty("filename", null);
+        verify(internalFilename, "For the BeanShellFilter a filename property must be defined.");
+        filename = IOUtils.getAbsoluteFile(getBaseDir(),internalFilename);
+
+        String initFilename = getProperty("initFilename", null);
+
         interpreter = new Interpreter();
         try {
             interpreter.set("instance", this);
+            interpreter.set("init", init);
+            if(!Strings.isNullOrEmpty(initFilename)) {
+                String absoluteInitFileName = IOUtils.getAbsoluteFile(getBaseDir(), initFilename);
+                interpreter.source(absoluteInitFileName);
+                init = (Map<String, Object>) interpreter.get("init");
+            }
         } catch (EvalError evalError) {
             throw new RuntimeException(evalError);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
         super.init();
 
