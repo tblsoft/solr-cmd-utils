f5f6be32e12bc08bcec2ad00b585863e7d329109
Thomas Bärwinkel <tbl@gmx.de>
2016-04-29 00:29:03 +0200

    add bean tests for Document

--- src/test/java/de/tblsoft/solr/pipeline/test/BeanTest.java
+++ src/test/java/de/tblsoft/solr/pipeline/test/BeanTest.java
@@ -1,5 +1,7 @@
 package de.tblsoft.solr.pipeline.test;
 
+import de.tblsoft.solr.pipeline.bean.Document;
+import de.tblsoft.solr.pipeline.bean.DocumentBuilder;
 import de.tblsoft.solr.pipeline.bean.Filter;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,6 +14,22 @@ public class BeanTest {
     @Test
     public void testFilterBean() {
         testBean(new Filter());
+    }
+
+    @Test
+    public void testDocumentBean() {
+        testBean(DocumentBuilder.document().create());
+    }
+
+
+    @Test
+    public void testDeleteFieldInDocument() {
+        Document document = DocumentBuilder.document().field("foo","bar").field("john", "doe").create();
+        document.deleteField("foo");
+        Assert.assertNull(document.getField("foo"));
+        Assert.assertEquals("doe", document.getFieldValue("john"));
+        Assert.assertEquals(1, document.getFields().size());
+
     }
 
     void testBean(Object bean) {
