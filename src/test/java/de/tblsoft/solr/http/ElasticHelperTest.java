c028bd73bce26b07392093ff85dda86e075fdbf9
Thomas Bärwinkel <tbl@gmx.de>
2016-04-30 15:00:12 +0200

    implement unit test for ElasticHelper

---
+++ src/test/java/de/tblsoft/solr/http/ElasticHelperTest.java
@@ -0,0 +1,47 @@
+package de.tblsoft.solr.http;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URISyntaxException;
+
+/**
+ * Created by tblsoft on 30.04.16.
+ */
+public class ElasticHelperTest {
+
+
+    @Test
+    public void getIndexUrlTest() throws URISyntaxException {
+        String expected = "http://localhost/foo";
+        String actual = ElasticHelper.getIndexUrl("http://localhost/foo");
+        Assert.assertEquals(expected, actual);
+
+        actual = ElasticHelper.getIndexUrl("http://localhost/foo/");
+        Assert.assertEquals(expected, actual);
+
+        actual = ElasticHelper.getIndexUrl("http://localhost/foo/bar");
+        Assert.assertEquals(expected, actual);
+
+        actual = ElasticHelper.getIndexUrl("http://localhost/foo/bar/");
+        Assert.assertEquals(expected, actual);
+
+        actual = ElasticHelper.getIndexUrl("http://localhost/foo/bar/?");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test(expected = URISyntaxException.class)
+    public void getIndexUrlTestWithEmptyUrl() throws URISyntaxException {
+        ElasticHelper.getIndexUrl("");
+    }
+
+    @Test(expected = URISyntaxException.class)
+    public void getIndexUrlTestWithNullUrl() throws URISyntaxException {
+        ElasticHelper.getIndexUrl(null);
+    }
+
+    @Test(expected = URISyntaxException.class)
+    public void getIndexUrlTestInvalidElasticIndexUrl() throws URISyntaxException {
+        ElasticHelper.getIndexUrl("http://localhost/");
+    }
+}
