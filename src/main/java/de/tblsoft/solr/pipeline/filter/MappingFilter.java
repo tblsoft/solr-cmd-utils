7974f7ad625adaf8f5289c7b9f50de861628d193
Thomas Bärwinkel <thomas.baerwinkel@hornbach.com>
2016-05-12 14:34:45 +0200

    extend mapping filter to allow the usage of simple functions

--- src/main/java/de/tblsoft/solr/pipeline/filter/MappingFilter.java
+++ src/main/java/de/tblsoft/solr/pipeline/filter/MappingFilter.java
@@ -1,70 +1,110 @@
 package de.tblsoft.solr.pipeline.filter;
 
-
-import com.google.common.base.Strings;
-import de.tblsoft.solr.pipeline.AbstractFilter;
-import de.tblsoft.solr.pipeline.bean.Document;
-import de.tblsoft.solr.pipeline.bean.Field;
-import org.apache.commons.lang3.text.StrSubstitutor;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.StrSubstitutor;
+
+import com.google.common.base.Strings;
+
+import de.tblsoft.solr.pipeline.AbstractFilter;
+import de.tblsoft.solr.pipeline.bean.Document;
+import de.tblsoft.solr.pipeline.bean.Field;
 
 public class MappingFilter extends AbstractFilter {
 
+	private Map<String, List<String>> mapping = new HashMap<String, List<String>>();
+	private Map<String, List<String>> mappingFunctions = new HashMap<String, List<String>>();
+	private Map<String, String> joins = new HashMap<String, String>();
 
+	@Override
+	public void init() {
 
-    private Map<String,String> mapping = new HashMap<String, String>();
-    private Map<String,String> joins = new HashMap<String, String>();
+		List<String> tempMapping = getPropertyAsList("mapping",
+				new ArrayList<String>());
+		for (String v : tempMapping) {
+			if (v.startsWith("join:")) {
+				v = v.replace("join:", "");
+				String[] s = v.split("=");
+				joins.put(s[0], s[1]);
+			} else {
+				String[] s = v.split("->");
 
+				String[] f = s[1].split(Pattern.quote("|"));
 
+				List<String> mappingList = mapping.get(s[0]);
+				if (mappingList == null) {
+					mappingList = new ArrayList<String>();
+				}
+				mappingList.add(f[0]);
+				mapping.put(s[0], mappingList);
+				List<String> functions = new ArrayList<String>();
+				for (int i = 1; i < f.length; i++) {
+					functions.add(f[i]);
+				}
+				mappingFunctions.put(f[0], functions);
+			}
+		}
+		super.init();
+	}
 
+	public String executeFunction(String function, String value) {
+		switch (function) {
+		case "md5":
+			return DigestUtils.md5Hex(value);
+		case "lowercase":
+			return StringUtils.lowerCase(value);
+		case "trim":
+			return StringUtils.trim(value);
+		}
+		throw new IllegalArgumentException("The function: " + function
+				+ " is not implemented.");
+	}
 
-    @Override
-    public void init() {
-        List<String> tempMapping = getPropertyAsList("mapping", new ArrayList<String>());
-        for(String v: tempMapping) {
-            if(v.startsWith("join:")) {
-                v = v.replace("join:", "");
-                String[] s = v.split("=");
-                joins.put(s[0], s[1]);
-            } else {
-                String[] s = v.split("->");
-                mapping.put(s[0], s[1]);
-            }
-        }
-        super.init();
-    }
+	@Override
+	public void document(Document document) {
+		Document mappedDocument = new Document();
+		for (Field f : document.getFields()) {
+			List<String> mappedNameList = mapping.get(f.getName());
+			if(mappedNameList == null) {
+				continue;
+			}
 
+			for (String mappedName : mappedNameList) {
+				List<String> mappedFunctions = mappingFunctions.get(mappedName);
+				if (!Strings.isNullOrEmpty(mappedName)) {
+					List<String> newValues = new ArrayList<String>();
+					for (String value : f.getValues()) {
+						String newValue = value;
+						for (String function : mappedFunctions) {
+							newValue = executeFunction(function, newValue);
+						}
+						newValues.add(newValue);
+					}
 
-    @Override
-    public void document(Document document) {
-        Document mappedDocument = new Document();
-        for(Field f : document.getFields()) {
-            String mappedName = mapping.get(f.getName());
-            if(!Strings.isNullOrEmpty(mappedName)) {
-                mappedDocument.setField(mappedName, f.getValues());
-            }
-        }
+					mappedDocument.setField(mappedName, newValues);
+				}
+			}
+		}
 
-        for(Map.Entry<String, String> entry: joins.entrySet()) {
-            Map<String, String> documentMap = new HashMap<String, String>();
-            for(Field field: document.getFields()) {
-                documentMap.put(field.getName(),field.getValue());
-            }
-            StrSubstitutor sub = new StrSubstitutor(documentMap);
-            String value = sub.replace(entry.getValue());
-            mappedDocument.addField(entry.getKey(), value);
-        }
+		for (Map.Entry<String, String> entry : joins.entrySet()) {
+			Map<String, String> documentMap = new HashMap<String, String>();
+			for (Field field : document.getFields()) {
+				documentMap.put(field.getName(), field.getValue());
+			}
+			StrSubstitutor sub = new StrSubstitutor(documentMap);
+			String value = sub.replace(entry.getValue());
+			mappedDocument.addField(entry.getKey(), value);
+		}
 
-        if(mappedDocument.getFields().size() != 0) {
-            super.document(mappedDocument);
-        }
-
-    }
+		if (mappedDocument.getFields().size() != 0) {
+			super.document(mappedDocument);
+		}
+	}
 
 }
-
--- src/test/java/de/tblsoft/solr/pipeline/filter/MappingFilterTest.java
+++ src/test/java/de/tblsoft/solr/pipeline/filter/MappingFilterTest.java
@@ -58,4 +58,41 @@ public class MappingFilterTest extends AbstractFilterTest {
         assertNumberOfDocuments(1);
         assertNumberOfFields(2);
     }
+    
+    @Test
+    public void testMappingTrimLowercaseMd5() {
+        configure();
+        addProperty("mapping", "noun->noun|trim|lowercase|md5");
+        createField("noun", " Substantiv ");
+        runTest();
+        assertFiled("noun", "0f194a56105d09847fffcdf8c9eaf979");
+        assertNumberOfDocuments(1);
+        assertNumberOfFields(1);
+    }
+    
+    @Test
+    public void testMappingForMultipleFields() {
+        configure();
+        addProperty("mapping", "noun->title");
+        addProperty("mapping", "noun->id|lowercase");
+        addProperty("mapping", "noun->noun|trim|lowercase|md5");
+        createField("noun", " Substantiv ");
+        runTest();
+        assertFiled("title", " Substantiv ");
+        assertFiled("id", " substantiv ");
+        assertFiled("noun", "0f194a56105d09847fffcdf8c9eaf979");
+        assertNumberOfDocuments(1);
+        assertNumberOfFields(3);
+    }
+    
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testMappingFunctionNotImplemented() {
+        configure();
+        addProperty("mapping", "noun->noun|lowercase|md5|unknown");
+        createField("noun", "Substantiv");
+        runTest();
+    }
+    
+    
 }
