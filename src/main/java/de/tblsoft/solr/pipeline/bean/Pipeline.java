41148b29ca396c58aef246ab952b183eaaa3fc78
Thomas Bärwinkel <tbl@gmx.de>
2016-05-27 23:03:28 +0200

    (#5) adding variables to the pipeline yaml file

--- src/main/java/de/tblsoft/solr/pipeline/bean/Pipeline.java
+++ src/main/java/de/tblsoft/solr/pipeline/bean/Pipeline.java
@@ -1,12 +1,15 @@
 package de.tblsoft.solr.pipeline.bean;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by oelbaer on 22.01.16.
  */
 public class Pipeline {
     private String name;
+
+    private Map<String,?> variables;
 
     private Reader reader;
     private List<Filter> filter;
@@ -36,10 +39,19 @@ public class Pipeline {
     }
 
 
+    public Map<String, ?> getVariables() {
+        return variables;
+    }
+
+    public void setVariables(Map<String, ?> variables) {
+        this.variables = variables;
+    }
+
     @Override
     public String toString() {
         return "Pipeline{" +
                 "name='" + name + '\'' +
+                ", variables=" + variables +
                 ", reader=" + reader +
                 ", filter=" + filter +
                 '}';
