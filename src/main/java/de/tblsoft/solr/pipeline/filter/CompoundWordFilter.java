81936cba3215264e98c0f67dd99b782dbfbd82fb
Thomas Bärwinkel <thomas.baerwinkel@hornbach.com>
2016-07-12 08:09:09 +0200

    improve the compound word filter

--- src/main/java/de/tblsoft/solr/pipeline/filter/CompoundWordFilter.java
+++ src/main/java/de/tblsoft/solr/pipeline/filter/CompoundWordFilter.java
@@ -1,11 +1,19 @@
 package de.tblsoft.solr.pipeline.filter;
 
+import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
+
+import de.hornbach.solr.helper.LinguisticHelper;
 import de.tblsoft.solr.pipeline.AbstractFilter;
 import de.tblsoft.solr.pipeline.bean.Document;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import sun.org.mozilla.javascript.internal.ast.ForInLoop;
 
 /**
  * Created by tblsoft 17.05.16.
@@ -14,7 +22,26 @@ public class CompoundWordFilter extends AbstractFilter {
 
     private String nounnFieldName;
 
-    private List<String> nounList = new ArrayList<String>();
+    private Set<String> nounList = new HashSet<String>();
+    
+    private static Set<String> blacklist = new HashSet<String>();
+    static {
+    	blacklist.add("euch");
+    	blacklist.add("herb");
+    }
+    
+    private static Set<String> whitelist = new HashSet<String>();
+    static {
+    	whitelist.add("leucht");
+    	whitelist.add("bohrung");
+    	whitelist.add("alu");
+    	whitelist.add("dampf");
+    	whitelist.add("kasten");
+    	whitelist.add("natrium");
+    	whitelist.add("bau");
+    	whitelist.add("leiste");
+    	whitelist.add("schutz");
+    }
 
     @Override
     public void init() {
@@ -25,13 +52,20 @@ public class CompoundWordFilter extends AbstractFilter {
     @Override
     public void document(Document document) {
         String noun = document.getFieldValue(nounnFieldName);
-        if(!Strings.isNullOrEmpty(noun)) {
-            nounList.add(noun.toLowerCase());
+        if(Strings.isNullOrEmpty(noun)) {
+        	return;
+        }
+        if(blacklist.contains(noun)) {
+        	return;
+        }
+        if(LinguisticHelper.containsOnlyGermanCharacters(noun) && noun.length() > 3) {
+        		nounList.add(noun.toLowerCase());
         }
     }
 
     @Override
     public void end() {
+    	nounList.addAll(whitelist);
         for(String noun: nounList) {
             List<String> compoundList = new ArrayList<String>();
             for(String compound: nounList) {
@@ -46,11 +80,69 @@ public class CompoundWordFilter extends AbstractFilter {
                 Document document = new Document();
                 document.addField("noun",noun);
                 for(String compound: compoundList) {
-                    document.addField("compound", compound);
+                	document.addField("compound", compound);
+//                	if(!isOverlap(compound, compoundList)) {
+//                		document.addField("compound", compound);
+//                	}
+                	
                 }
+                document.addField("tokenized", tokenize(noun, compoundList));
                 super.document(document);
             }
         }
         super.end();
+    }
+    
+    List<String> tokenize(String noun, List<String> compoundList) {
+    	if("blumenzwiebel".equals(noun)) {
+    		System.out.println("test");
+    	}
+    	StringBuilder ret = new StringBuilder();
+    	StringBuilder b = new StringBuilder();
+
+    	for (int i = 0; i < noun.length(); i++) {
+			b.append(noun.charAt(i));
+			String part = b.toString();
+			String candidate = null;
+			boolean startsWith = false;
+			for(String compound : compoundList) {
+				if(compound.equals(part)) {
+					candidate = compound;
+					continue;
+				}
+				if(compound.startsWith(part)) {
+					startsWith = true;
+				}
+			}
+			if(candidate != null && startsWith == false) {
+				ret.append(part).append(" ");
+				candidate = null;
+				b = new StringBuilder();
+				continue;
+				
+			}
+			if(startsWith == false) {
+				if(part.length() > 1) {
+					i = i - part.length() +1;
+				}
+				b = new StringBuilder();
+			}
+			
+		}
+    	return ret.toString().trim();
+    }
+    
+    boolean isOverlap(String compoundToCompare, List<String> compoundList) {
+    	for(String compound: compoundList) {
+    		if(compoundToCompare.equals(compound)) {
+    			continue;
+    		}
+    		if(compound.contains(compoundToCompare)) {
+    			return true;
+    		}
+    		
+    	}
+    	return false;
+    	
     }
 }
--- src/test/java/de/tblsoft/solr/pipeline/filter/CompoundWordFilterTest.java
+++ src/test/java/de/tblsoft/solr/pipeline/filter/CompoundWordFilterTest.java
@@ -1,8 +1,11 @@
 package de.tblsoft.solr.pipeline.filter;
 
-import de.tblsoft.solr.pipeline.bean.DocumentBuilder;
-import de.tblsoft.solr.pipeline.test.AbstractFilterTest;
 import org.junit.Test;
+
+import de.tblsoft.solr.pipeline.bean.Document;
+import de.tblsoft.solr.pipeline.bean.DocumentBuilder;
+import de.tblsoft.solr.pipeline.bean.Field;
+import de.tblsoft.solr.pipeline.test.AbstractFilterTest;
 
 /**
  * Created by tblsoft on 17.05.16.
@@ -13,7 +16,50 @@ public class CompoundWordFilterTest extends AbstractFilterTest {
     public void configure() {
         setClazz(CompoundWordFilter.class);
     }
+    
+    
+    @Test
+    public void testLeitungschutzschalter() {
+        configure();
+        document(
+                DocumentBuilder.document().field("noun","leitungsschutzschalter").create(),
+                DocumentBuilder.document().field("noun","leitung").create(),
+                DocumentBuilder.document().field("noun","schutz").create(),
+                DocumentBuilder.document().field("noun","schalter").create()
+        );
+        Field d =outputDocumentList.get(0).getField("tokenized");
+        assertFiledList("tokenized", "maurer", "hammer");
 
+    }
+
+    @Test
+    public void testTurmdeckelschnecke() {
+        configure();
+        document(
+                DocumentBuilder.document().field("noun","turmdeckelschnecke").create(),
+                DocumentBuilder.document().field("noun","turm").create(),
+                DocumentBuilder.document().field("noun","schnecke").create(),
+                DocumentBuilder.document().field("noun","blumen").create(),
+                DocumentBuilder.document().field("noun","blume").create()
+        );
+        Field d =outputDocumentList.get(0).getField("tokenized");
+        assertFiledList("tokenized", "maurer", "hammer");
+
+    }
+    @Test
+    public void testBlumenzwiebel() {
+    	configure();
+    	document(
+    			DocumentBuilder.document().field("noun","blumenzwiebel").create(),
+    			DocumentBuilder.document().field("noun","lumen").create(),
+    			DocumentBuilder.document().field("noun","zwiebel").create(),
+    			DocumentBuilder.document().field("noun","deckel").create()
+    			);
+        Document d =outputDocumentList.get(0);
+    	assertFiledList("tokenized", "maurer", "hammer");
+    	
+    }
+    
     @Test
     public void testCompoundWordFilter() {
         configure();
