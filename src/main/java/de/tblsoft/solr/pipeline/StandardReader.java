0824d3553f619ed34721a1c401ebd6dff47d49f6
Thomas Bärwinkel <thomas.baerwinkel@hornbach.com>
2016-10-27 22:08:58 +0200

    add the possibilty to read solr xml directly from a http endpoint

--- src/main/java/de/tblsoft/solr/pipeline/StandardReader.java
+++ src/main/java/de/tblsoft/solr/pipeline/StandardReader.java
@@ -1,11 +1,20 @@
 package de.tblsoft.solr.pipeline;
 
+import de.tblsoft.solr.http.HTTPHelper;
 import de.tblsoft.solr.parser.SolrXmlParser;
 import de.tblsoft.solr.pipeline.bean.Document;
 import de.tblsoft.solr.pipeline.bean.Reader;
 import de.tblsoft.solr.util.IOUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.commons.lang3.text.StrSubstitutor;
+
+import com.google.common.base.Strings;
 
 /**
  * Created by tblsoft on 23.01.16.
@@ -19,6 +28,10 @@ public class StandardReader extends SolrXmlParser implements ReaderIF {
     private String baseDir;
 
     private Document document = new Document();
+    
+    private File tempFile;
+    
+    protected Map<String,String> variables = new HashMap<String, String>();
 
     @Override
     public void field(String name, String value) {
@@ -36,7 +49,18 @@ public class StandardReader extends SolrXmlParser implements ReaderIF {
     @Override
     public void read() {
         try {
-            String filename = (String) reader.getProperty().get("filename");
+            
+        	String filename = (String) reader.getProperty().get("filename");
+        	String url = getProperty("url", null);
+        	if(!Strings.isNullOrEmpty(url)) {
+        		tempFile = File.createTempFile("solr-reader-temp-file", ".xml");
+        		System.out.println("Store temporary file to " + tempFile.getAbsolutePath());
+        		HTTPHelper.get2File(url, tempFile);
+        		filename = tempFile.getAbsolutePath();
+        	}
+            
+        	
+            
             String absoluteFilename = IOUtils.getAbsoluteFile(baseDir, filename);
             setInputFileName(absoluteFilename);
             parse();
@@ -56,6 +80,10 @@ public class StandardReader extends SolrXmlParser implements ReaderIF {
 
     @Override
     public void end() {
+    	try {
+			Files.deleteIfExists(tempFile.toPath());
+		} catch (IOException e) {
+			throw new RuntimeException(e);		}
 
     }
 
@@ -70,6 +98,21 @@ public class StandardReader extends SolrXmlParser implements ReaderIF {
 
     @Override
     public void setVariables(Map<String, String> variables) {
-
+        if(variables == null) {
+            return;
+        }
+        for(Map.Entry<String,String> entry: variables.entrySet()) {
+            this.variables.put("variables." + entry.getKey(), entry.getValue());
+        }
+    }
+    
+    public String getProperty(String name, String defaultValue) {
+        String value = (String) reader.getProperty().get(name);
+        if(value != null) {
+            StrSubstitutor strSubstitutor = new StrSubstitutor(variables);
+            value = strSubstitutor.replace(value);
+            return value;
+        }
+        return defaultValue;
     }
 }
