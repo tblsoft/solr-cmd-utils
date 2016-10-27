de4d7b07623f097c5a5fcb03eac91e4012917f8e
Thomas Bärwinkel <thomas.baerwinkel@hornbach.com>
2016-10-27 22:05:57 +0200

    add date util methods

--- src/main/java/de/tblsoft/solr/util/DateUtils.java
+++ src/main/java/de/tblsoft/solr/util/DateUtils.java
@@ -8,8 +8,7 @@ public class DateUtils {
 	
 	public static String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
 	
-	String date2String(Date date) {
-
+	public static String date2String(Date date) {
 		SimpleDateFormat sdfPipeline = new SimpleDateFormat(
 				"yyyy-MM-dd'T'HH:mm:ssZ");
 		return sdfPipeline.format(date);
@@ -19,6 +18,23 @@ public class DateUtils {
 	public static Date getDate(String date) {
 		SimpleDateFormat sdfPipeline = new SimpleDateFormat(
 				"yyyy-MM-dd'T'HH:mm:ssZ");
+		try {
+			return sdfPipeline.parse(date);
+		} catch (ParseException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static String date2String(Date date, String format) {
+		SimpleDateFormat sdfPipeline = new SimpleDateFormat(
+				format);
+		return sdfPipeline.format(date);
+
+	}
+	
+	public static Date getDate(String date, String format) {
+		SimpleDateFormat sdfPipeline = new SimpleDateFormat(
+				format);
 		try {
 			return sdfPipeline.parse(date);
 		} catch (ParseException e) {
