049a7d482b61e08351505d5bebb7c5a22f67faa5
Thomas Bärwinkel <tbl@gmx.de>
2017-02-02 22:05:11 +0100

    add a new method to transform date objects to solr date

--- src/main/java/de/tblsoft/solr/util/DateUtils.java
+++ src/main/java/de/tblsoft/solr/util/DateUtils.java
@@ -1,5 +1,10 @@
 package de.tblsoft.solr.util;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -7,7 +12,9 @@ import java.util.Date;
 public class DateUtils {
 	
 	public static String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
-	
+
+	public static String SOLR_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+
 	public static String date2String(Date date) {
 		SimpleDateFormat sdfPipeline = new SimpleDateFormat(
 				"yyyy-MM-dd'T'HH:mm:ssZ");
@@ -41,6 +48,12 @@ public class DateUtils {
 			throw new RuntimeException(e);
 		}
 		
+	}
+
+	public static String toSolrDate(String date) {
+		DateTime jodaDate = new DateTime(date);
+		DateTimeFormatter formatter = DateTimeFormat.forPattern(SOLR_DATE_FORMAT).withZone(DateTimeZone.UTC);
+		return jodaDate.toString(formatter);
 	}
 
 }
