78228f64a96f1fd974cc1ef86c4b4d0d7ba62906
Thomas Bärwinkel <tbl@gmx.de>
2017-09-23 20:27:02 +0200

    refactor mapping filter

--- src/main/java/de/tblsoft/solr/pipeline/filter/MappingFilter.java
+++ src/main/java/de/tblsoft/solr/pipeline/filter/MappingFilter.java
@@ -1,27 +1,14 @@
 package de.tblsoft.solr.pipeline.filter;
 
 import com.google.common.base.Strings;
-
-import de.tblsoft.solr.http.UrlUtil;
 import de.tblsoft.solr.pipeline.AbstractFilter;
 import de.tblsoft.solr.pipeline.bean.Document;
 import de.tblsoft.solr.pipeline.bean.Field;
 import de.tblsoft.solr.pipeline.bean.FieldComperator;
-
-import de.tblsoft.solr.util.DateUtils;
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.StrSubstitutor;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.regex.Pattern;
 
 public class MappingFilter extends AbstractFilter {
 
@@ -32,87 +19,21 @@ public class MappingFilter extends AbstractFilter {
     private boolean sortFieldsByName = false;
     private boolean addEmptyFieldIfNotExists = false;
 
+    private SimpleMapping simpleMapping;
 
 	@Override
 	public void init() {
 
         sortFieldsByName = getPropertyAsBoolean("sortFieldsByName", false);
         addEmptyFieldIfNotExists = getPropertyAsBoolean("addEmptyFieldIfNotExists", false);
+		simpleMapping = new SimpleMapping(getPropertyAsList("mapping", new ArrayList<String>()));
+		mapping = simpleMapping.getMapping();
+		mappingFunctions = simpleMapping.getMappingFunctions();
+		joins = simpleMapping.getJoins();
 
-		List<String> tempMapping = getPropertyAsList("mapping",
-				new ArrayList<String>());
-		for (String v : tempMapping) {
-			if (v.startsWith("join:")) {
-				v = v.replace("join:", "");
-				String[] s = v.split("=",2);
-				joins.put(s[0], s[1]);
-			} else {
-				String[] s = v.split("->");
-
-				String[] f = s[1].split(Pattern.quote("|"));
-
-				List<String> mappingList = mapping.get(s[0]);
-				if (mappingList == null) {
-					mappingList = new ArrayList<String>();
-				}
-				mappingList.add(f[0]);
-				mapping.put(s[0], mappingList);
-				List<String> functions = new ArrayList<String>();
-				for (int i = 1; i < f.length; i++) {
-					functions.add(f[i]);
-				}
-				mappingFunctions.put(f[0], functions);
-			}
-		}
 		super.init();
 	}
 
-	public String executeFunction(String function, String value) {
-
-        if("md5".equals(function)) {
-            return DigestUtils.md5Hex(value);
-        } else if ("lowercase".equals(function)) {
-            return StringUtils.lowerCase(value);
-        } else if ("urlencode".equals(function)) {
-            return UrlUtil.encode(value);
-        } else if ("urldecode".equals(function)) {
-            return UrlUtil.decode(value);
-        } else if ("trim".equals(function)) {
-            return StringUtils.trim(value);
-        } else if ("toSolrDate".equals(function)) {
-			return DateUtils.toSolrDate(value);
-		} else if ("uniq".equals(function)) {
-			return value;
-		}
-
-		throw new IllegalArgumentException("The function: " + function
-				+ " is not implemented.");
-	}
-
-
-	public void executeFieldFunction(String function, Field field) {
-
-		if("md5".equals(function)) {
-			return;
-		} else if ("lowercase".equals(function)) {
-			return;
-		} else if ("urlencode".equals(function)) {
-			return;
-		} else if ("urldecode".equals(function)) {
-			return;
-		} else if ("trim".equals(function)) {
-			return;
-		} else if ("toSolrDate".equals(function)) {
-			return;
-		} else if ("uniq".equals(function)) {
-			Set<String> uniqValues= new HashSet<String>();
-			uniqValues.addAll(field.getValues());
-			field.setValues(new ArrayList(uniqValues));
-			return;
-		}
-		throw new IllegalArgumentException("The function: " + function
-				+ " is not implemented.");
-	}
 
 	@Override
 	public void document(Document document) {
@@ -130,7 +51,7 @@ public class MappingFilter extends AbstractFilter {
 					for (String value : f.getValues()) {
 						String newValue = value;
 						for (String function : mappedFunctions) {
-							newValue = executeFunction(function, newValue);
+							newValue = simpleMapping.executeFunction(function, newValue);
 						}
 						newValues.add(newValue);
 					}
@@ -138,7 +59,7 @@ public class MappingFilter extends AbstractFilter {
 					mappedDocument.addField(mappedName, newValues);
 					Field mappedField = mappedDocument.getField(mappedName);
 					for (String function : mappedFunctions) {
-						executeFieldFunction(function, mappedField);
+						simpleMapping.executeFieldFunction(function, mappedField);
 					}
 				}
 			}
---
+++ src/main/java/de/tblsoft/solr/pipeline/filter/SimpleMapping.java
@@ -0,0 +1,117 @@
+package de.tblsoft.solr.pipeline.filter;
+
+import de.tblsoft.solr.http.UrlUtil;
+import de.tblsoft.solr.pipeline.bean.Field;
+import de.tblsoft.solr.util.DateUtils;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+/**
+ * Created by tblsoft on 23.09.17.
+ */
+public class SimpleMapping {
+
+
+    private Map<String, List<String>> mapping = new HashMap<String, List<String>>();
+    private Map<String, List<String>> mappingFunctions = new HashMap<String, List<String>>();
+    private Map<String, String> joins = new HashMap<String, String>();
+
+    private List<String> mappingConfiguration;
+
+    public SimpleMapping(List<String> mappingConfiguration) {
+        this.mappingConfiguration = mappingConfiguration;
+        readConfig();
+    }
+
+    private void readConfig() {
+
+
+        for (String v : mappingConfiguration) {
+            if (v.startsWith("join:")) {
+                v = v.replace("join:", "");
+                String[] s = v.split("=", 2);
+                joins.put(s[0], s[1]);
+            } else {
+                String[] s = v.split("->");
+
+                String[] f = s[1].split(Pattern.quote("|"));
+
+                List<String> mappingList = mapping.get(s[0]);
+                if (mappingList == null) {
+                    mappingList = new ArrayList<String>();
+                }
+                mappingList.add(f[0]);
+                mapping.put(s[0], mappingList);
+                List<String> functions = new ArrayList<String>();
+                for (int i = 1; i < f.length; i++) {
+                    functions.add(f[i]);
+                }
+                mappingFunctions.put(f[0], functions);
+            }
+        }
+    }
+
+
+    public String executeFunction(String function, String value) {
+
+        if("md5".equals(function)) {
+            return DigestUtils.md5Hex(value);
+        } else if ("lowercase".equals(function)) {
+            return StringUtils.lowerCase(value);
+        } else if ("urlencode".equals(function)) {
+            return UrlUtil.encode(value);
+        } else if ("urldecode".equals(function)) {
+            return UrlUtil.decode(value);
+        } else if ("trim".equals(function)) {
+            return StringUtils.trim(value);
+        } else if ("toSolrDate".equals(function)) {
+            return DateUtils.toSolrDate(value);
+        } else if ("uniq".equals(function)) {
+            return value;
+        }
+
+        throw new IllegalArgumentException("The function: " + function
+                + " is not implemented.");
+    }
+
+
+    public void executeFieldFunction(String function, Field field) {
+
+        if("md5".equals(function)) {
+            return;
+        } else if ("lowercase".equals(function)) {
+            return;
+        } else if ("urlencode".equals(function)) {
+            return;
+        } else if ("urldecode".equals(function)) {
+            return;
+        } else if ("trim".equals(function)) {
+            return;
+        } else if ("toSolrDate".equals(function)) {
+            return;
+        } else if ("uniq".equals(function)) {
+            Set<String> uniqValues= new HashSet<String>();
+            uniqValues.addAll(field.getValues());
+            field.setValues(new ArrayList(uniqValues));
+            return;
+        }
+        throw new IllegalArgumentException("The function: " + function
+                + " is not implemented.");
+    }
+
+
+    public Map<String, List<String>> getMapping() {
+        return mapping;
+    }
+
+    public Map<String, List<String>> getMappingFunctions() {
+        return mappingFunctions;
+    }
+
+    public Map<String, String> getJoins() {
+        return joins;
+    }
+}
