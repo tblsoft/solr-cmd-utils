9b77c0b62898a11cce5f151260958ba225032193
Thomas Bärwinkel <tbl@gmx.de>
2017-11-20 19:35:58 +0100

    increase the version to 2.0-SNAPSHOT

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
-mvn clean install
-tar xvfz target/solr-cmd-utils-1.0-SNAPSHOT-cmd.tar.gz -C ~/dev
+mvn clean install -DskipTests=true
+rm -rf ~/dev/solr-cmd-utils/
+tar xvfz target/solr-cmd-utils-2.0-SNAPSHOT-cmd.tar.gz -C ~/dev
