ecbfd9c925324341a00d6c3dac8d262c72c96424
mik <marius.kolkowski@raystorm.place>
2017-11-24 18:12:35 +0100

    - add mapFields function

--- src/main/java/de/tblsoft/solr/util/DocumentUtils.java
+++ src/main/java/de/tblsoft/solr/util/DocumentUtils.java
@@ -1,5 +1,11 @@
 package de.tblsoft.solr.util;
 
+import de.tblsoft.solr.pipeline.bean.Document;
+import de.tblsoft.solr.pipeline.bean.Field;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class DocumentUtils {
 
 	public static String normalizeFieldKey(String key) {
@@ -8,4 +14,12 @@ public class DocumentUtils {
 		return key;
 	}
 
+	public static Map<String, Field> mapFields(Document document) {
+		Map<String, Field> fields = new HashMap<String, Field>();
+		for (Field field : document.getFields()) {
+			fields.put(field.getName(), field);
+		}
+
+		return fields;
+	}
 }
