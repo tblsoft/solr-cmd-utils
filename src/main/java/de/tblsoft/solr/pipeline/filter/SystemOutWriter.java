fd9d355b4109fa659118534d060c272fdeaa99e1
mik <marius.kolkowski@raystorm.place>
2017-11-28 16:19:15 +0100

    - fix nullptr

--- src/main/java/de/tblsoft/solr/pipeline/filter/SystemOutWriter.java
+++ src/main/java/de/tblsoft/solr/pipeline/filter/SystemOutWriter.java
@@ -38,7 +38,7 @@ public class SystemOutWriter extends AbstractFilter {
                 fieldCounter++;
                 System.out.print(prefix+"name: " + f.getName());
 
-                String out = Joiner.on(", ").join(f.getValues());
+                String out = Joiner.on(", ").skipNulls().join(f.getValues());
                 System.out.println(prefix+" -- value: " + out );
             }
         }
