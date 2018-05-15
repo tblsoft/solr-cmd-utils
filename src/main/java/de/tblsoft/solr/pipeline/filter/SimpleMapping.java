8f2afcd6739f6711dba81d1bf938187fcdc9c57b
Kolkowski, Marius <marius.kolkowski@cgm.com>
2018-05-15 14:17:24 +0200

    - add france special characters mapping

--- src/main/java/de/tblsoft/solr/pipeline/filter/SimpleMapping.java
+++ src/main/java/de/tblsoft/solr/pipeline/filter/SimpleMapping.java
@@ -65,6 +65,8 @@ public class SimpleMapping {
             return DigestUtils.md5Hex(value);
         } else if ("mapGermanChars".equals(function)) {
             return mapGermanChars(value);
+        } else if ("mapFranceChars".equals(function)) {
+            return mapFranceChars(value);
         } else if ("lowercase".equals(function)) {
             return StringUtils.lowerCase(value);
         } else if ("urlencode".equals(function)) {
@@ -89,6 +91,8 @@ public class SimpleMapping {
         if("md5".equals(function)) {
             return;
         } else if ("mapGermanChars".equals(function)) {
+            return;
+        } else if ("mapFranceChars".equals(function)) {
             return;
         }else if ("lowercase".equals(function)) {
             return;
@@ -132,6 +136,37 @@ public class SimpleMapping {
         value = value.replaceAll("\u00f6", "oe");
         value = value.replaceAll("\u00fc", "ue");
         value = value.replaceAll("\u00df", "ss");
+        return value;
+    }
+
+    static String mapFranceChars(String value) {
+        value = value.replaceAll("\u00c8", "E"); // È
+        value = value.replaceAll("\u00e8", "e"); // è
+        value = value.replaceAll("\u00c9", "E"); // É
+        value = value.replaceAll("\u00e9", "e"); // é
+        value = value.replaceAll("é", "e"); // é
+        value = value.replaceAll("\u00c0", "A"); // À
+        value = value.replaceAll("\u00e0", "a"); // à
+        value = value.replaceAll("\u00d9", "U"); // Ù
+        value = value.replaceAll("\u00f9", "u"); // ù
+        value = value.replaceAll("\u00c2", "A"); // Â
+        value = value.replaceAll("\u00e2", "a"); // â
+        value = value.replaceAll("\u00ca", "E"); // Ê
+        value = value.replaceAll("\u00ea", "e"); // ê
+        value = value.replaceAll("\u00ce", "I"); // Î
+        value = value.replaceAll("\u00ee", "i"); // î
+        value = value.replaceAll("\u00d4", "O"); // Ô
+        value = value.replaceAll("\u00f4", "o"); // ô
+        value = value.replaceAll("\u00db", "U"); // Û
+        value = value.replaceAll("\u00fb", "u"); // û
+        value = value.replaceAll("\u00c7", "C"); // Ç
+        value = value.replaceAll("\u00e7", "c"); // ç
+        value = value.replaceAll("\u00cb", "E"); // Ë
+        value = value.replaceAll("\u00eb", "e"); // ë
+        value = value.replaceAll("\u00cf", "I"); // Ï
+        value = value.replaceAll("\u00ee", "i"); // î
+        value = value.replaceAll("\u00dc", "U"); // Ü
+        value = value.replaceAll("\u00fb", "u"); // û
         return value;
     }
 }
