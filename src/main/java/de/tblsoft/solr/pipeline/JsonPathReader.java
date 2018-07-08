c6453822a11a4e1fdfb24e44c85d4d7bcc34b2ed
Thomas Bärwinkel <tbl@gmx.de>
2018-07-08 23:44:34 +0200

    add a mapping to the json path reader

--- src/main/java/de/tblsoft/solr/pipeline/JsonPathReader.java
+++ src/main/java/de/tblsoft/solr/pipeline/JsonPathReader.java
@@ -1,5 +1,6 @@
 package de.tblsoft.solr.pipeline;
 
+import com.google.api.client.repackaged.com.google.common.base.Strings;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.PathNotFoundException;
@@ -8,7 +9,6 @@ import de.tblsoft.solr.pipeline.bean.Document;
 import de.tblsoft.solr.pipeline.bean.Reader;
 import de.tblsoft.solr.pipeline.filter.SimpleMapping;
 import de.tblsoft.solr.util.IOUtils;
-import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,50 +21,71 @@ import java.util.Map;
  */
 public class JsonPathReader extends AbstractReader {
 
+    private Map<String, List<String>> mapping;
+    private String rootPath;
+
     public void read() {
         SimpleMapping simpleMapping = new SimpleMapping(getPropertyAsList("mapping", new ArrayList<String>()), getPropertyAsList("config", new ArrayList<String>()));
-        Map<String, List<String>> mapping = simpleMapping.getMapping();
+        mapping = simpleMapping.getMapping();
         try {
-            String rootPath = getProperty("rootPath", "$");
+            rootPath = getProperty("rootPath", "$");
+            String url = getProperty("url", null);
+            String path = getProperty("path", null);
+            String filename = getProperty("filename", null);
 
-            DocumentContext context = loadJsonContext();
-            List<Object> jsonHits = context.read(rootPath);
-            for(Object obj: jsonHits){
-                Document document = new Document();
-
-                for(Map.Entry<String, List<String>> mappingEntry : mapping.entrySet()) {
-                    try {
-                        Object parsedValue = JsonPath.parse(obj).read(mappingEntry.getKey());
-                        for(String target: mappingEntry.getValue()) {
-                            document.setField(target, parsedValue);
-                        }
-                    } catch (PathNotFoundException e) {
-                        //ignore
-                    }
+            if(!Strings.isNullOrEmpty(url)) {
+                DocumentContext context = loadJsonContextFromUrl(url);
+                execute(context, url);
+            } else if(!Strings.isNullOrEmpty(filename)) {
+                File jsonFile = IOUtils.getAbsoluteFileAsFile(getBaseDir(), filename);
+                DocumentContext context = loadJsonContextFromFile(jsonFile);
+                execute(context, filename);
+            } else if(!Strings.isNullOrEmpty(path)) {
+                String absolutePath = IOUtils.getAbsoluteFile(getBaseDir(), path);
+                List<String> absoluteJsonFiles = IOUtils.getFiles(absolutePath);
+                for(String absoluteJsonFile : absoluteJsonFiles) {
+                    File file = new File(absoluteJsonFile);
+                    DocumentContext context = loadJsonContextFromFile(new File(absoluteJsonFile));
+                    execute(context, file.getName());
                 }
-                executer.document(document);
             }
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    protected DocumentContext loadJsonContext() throws IOException {
-        DocumentContext context = null;
 
-        String url = getProperty("url", null);
-        String filename = getProperty("filename", null);
+    private void execute(DocumentContext context, String source) {
+        List<Object> jsonHits = context.read(rootPath);
+        for(Object obj: jsonHits){
+            Document document = new Document();
 
-        if(StringUtils.isNotEmpty(url)) {
-            //@todo jsonBody has encoding bug
-            String jsonBody = HTTPHelper.get(url);
-            context = JsonPath.parse(jsonBody);
+            for(Map.Entry<String, List<String>> mappingEntry : mapping.entrySet()) {
+                try {
+                    Object parsedValue = JsonPath.parse(obj).read(mappingEntry.getKey());
+                    for(String target: mappingEntry.getValue()) {
+                        document.setField(target, parsedValue);
+                    }
+                } catch (PathNotFoundException e) {
+                    //ignore
+                }
+            }
+            document.addField("source", source);
+            executer.document(document);
         }
-        else if(StringUtils.isNotEmpty(filename)) {
-            File jsonFile = IOUtils.getAbsoluteFileAsFile(getBaseDir(), filename);
-            context = JsonPath.parse(jsonFile);
-        }
+    }
 
+    protected DocumentContext loadJsonContextFromUrl(String url) throws IOException {
+        //@todo jsonBody has encoding bug
+        String jsonBody = HTTPHelper.get(url);
+        DocumentContext context = JsonPath.parse(jsonBody);
+        return context;
+    }
+
+
+    protected DocumentContext loadJsonContextFromFile(File jsonFile) throws IOException {
+        DocumentContext context = JsonPath.parse(jsonFile);
         return context;
     }
 
