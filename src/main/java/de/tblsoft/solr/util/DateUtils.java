b26664b62b89617d4d2138dbe8e55a28885b3830
Thomas Bärwinkel <tbl@gmx.de>
2019-01-28 21:26:29 +0100

    use a constant for date pattern

--- src/main/java/de/tblsoft/solr/util/DateUtils.java
+++ src/main/java/de/tblsoft/solr/util/DateUtils.java
@@ -4,9 +4,12 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import sun.text.resources.cldr.ti.FormatData_ti_ER;
 
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.ZoneId;
 import java.util.Date;
 
 public class DateUtils {
@@ -15,11 +18,17 @@ public class DateUtils {
 
 	public static String SOLR_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
 
+	public static java.time.format.DateTimeFormatter FORMATTER = java.time.format.DateTimeFormatter.ofPattern(DATE_FORMAT);
+
 	public static String date2String(Date date) {
 		SimpleDateFormat sdfPipeline = new SimpleDateFormat(
-				"yyyy-MM-dd'T'HH:mm:ssZ");
+				DATE_FORMAT);
 		return sdfPipeline.format(date);
 
+	}
+
+	public static String timestamp2String(Timestamp timestamp, String timezone) {
+		return timestamp.toInstant().atZone(ZoneId.of(timezone)).format(FORMATTER);
 	}
 	
 	public static Date getDate(String date) {
