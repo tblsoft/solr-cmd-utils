38e83503079bd924376eedc8ba548c2cf6d833cd
Pannous <info@pannous.net>
2019-01-30 19:40:41 +0100

    N3Writer teak

--- src/main/java/de/tblsoft/solr/pipeline/filter/N3Writer.java
+++ src/main/java/de/tblsoft/solr/pipeline/filter/N3Writer.java
@@ -13,64 +13,61 @@ import java.io.OutputStream;
  */
 public class N3Writer extends AbstractFilter {
 
-    private String absoluteFilename;
+	private String absoluteFilename;
 
-    private OutputStream outputStream;
+	private OutputStream outputStream;
 
-    private OutputStreamStringBuilder outputStreamStringBuilder;
-
-
-    @Override
-    public void init() {
+	private OutputStreamStringBuilder outputStreamStringBuilder;
 
 
+	@Override
+	public void init() {
+		String relativeFilename = getProperty("filename", null);
+		absoluteFilename = IOUtils.getAbsoluteFile(getBaseDir(), relativeFilename);
+
+		verify(absoluteFilename, "For the FileLineWriter a filname must be defined.");
+
+		try {
+			outputStream = IOUtils.getOutputStream(absoluteFilename);
+			outputStreamStringBuilder = new OutputStreamStringBuilder(outputStream);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		super.init();
+
+	}
+
+	@Override
+	public void document(Document document) {
+		try {
+			String subject = document.getFieldValue("subject");
+			String predicate = document.getFieldValue("predicate");
+			String object = document.getFieldValue("object");
+			subject=subject.replaceAll(" ", "_");
+			predicate=predicate.replaceAll(" ", "_");
+
+			outputStreamStringBuilder.append("<").append(subject).append(">").append("\t");
+			outputStreamStringBuilder.append("<").append(predicate).append(">").append("\t");
+			outputStreamStringBuilder.append("\"").append(object).append("\"");
+			outputStreamStringBuilder.append("\n");
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		super.document(document);
+	}
 
 
-            String relativeFilename = getProperty("filename", null);
-            absoluteFilename = IOUtils.getAbsoluteFile(getBaseDir(), relativeFilename);
-
-            verify(absoluteFilename, "For the FileLineWriter a filname must be defined.");
-
-            try {
-                outputStream = IOUtils.getOutputStream(absoluteFilename);
-                outputStreamStringBuilder = new OutputStreamStringBuilder(outputStream);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            super.init();
-
-        }
-
-    @Override
-    public void document(Document document) {
-        try {
-            String subject = document.getFieldValue("subject");
-            String predicate = document.getFieldValue("predicate");
-            String object = document.getFieldValue("object");
-
-            outputStreamStringBuilder.append("<").append(subject).append(">").append("\t");
-            outputStreamStringBuilder.append("<").append(predicate).append(">").append("\t");
-            outputStreamStringBuilder.append("\"").append(object).append("\"");
-            outputStreamStringBuilder.append("\n");
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        super.document(document);
-    }
-
-
-
-    @Override
-    public void end() {
-        try {
-            outputStream.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        super.end();
-    }
+	@Override
+	public void end() {
+		try {
+			outputStream.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		super.end();
+	}
 
 }
