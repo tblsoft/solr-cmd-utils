2cf87020c9386c65e4142230ae913edc5da10ab8
Thomas Bärwinkel <tbl@gmx.de>
2019-05-03 14:27:46 +0200

    use the newest version for the install skript

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 set -e
 mvn clean install -DskipTests=true
 rm -rf ~/dev/solr-cmd-utils/
-tar xvfz target/solr-cmd-utils-3.2-SNAPSHOT-cmd.tar.gz -C ~/dev
+tar xvfz target/solr-cmd-utils-3.3-SNAPSHOT-cmd.tar.gz -C ~/dev
