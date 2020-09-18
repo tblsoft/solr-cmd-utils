544cfcbb5f6a57c313fd41d19af6cc7e4bf2ef60
Marius Kolkowski <marius.kolkowski@raystorm.place>
2020-09-18 12:33:01 +0200

    add RichJavascriptFilter

---
+++ src/main/java/de/tblsoft/solr/pipeline/filter/RichJavaScriptFilter.java
@@ -0,0 +1,75 @@
+package de.tblsoft.solr.pipeline.filter;
+
+import de.tblsoft.solr.pipeline.AbstractFilter;
+import de.tblsoft.solr.pipeline.bean.Document;
+import de.tblsoft.solr.pipeline.bean.DocumentBuilder;
+import de.tblsoft.solr.util.IOUtils;
+import org.apache.commons.io.FileUtils;
+import org.jsoup.parser.Parser;
+
+import javax.script.Compilable;
+import javax.script.CompiledScript;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RichJavaScriptFilter extends AbstractFilter {
+    private String filename;
+
+    private String script;
+
+    private ScriptEngine engine;
+
+    private CompiledScript compiledScript;
+
+    @Override
+    public void init() {
+        String internalFilename = getProperty("filename", null);
+        verify(internalFilename, "For the RichJavaScriptFilter a filename property must be defined.");
+        filename = IOUtils.getAbsoluteFile(getBaseDir(),internalFilename);
+        script = getProperty("script", null);
+
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        engine = mgr.getEngineByName("JavaScript");
+
+
+        try {
+            if(filename != null) {
+                script = FileUtils.readFileToString(new File(filename));
+            }
+            Compilable compEngine = (Compilable) engine;
+            compiledScript = compEngine.compile(script);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        super.init();
+    }
+
+    @Override
+    public void document(Document document) {
+        List<Document> docs = new ArrayList<>();
+        docs.add(document);
+        try {
+            engine.put("htmlParser", Parser.htmlParser());
+            engine.put("xmlParser", Parser.xmlParser());
+            engine.put("docs", docs);
+            engine.put("documentBuilder", new DocumentBuilder());
+            compiledScript.eval();
+
+            for(Document d : docs) {
+                super.document(d);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void end() {
+        super.end();
+    }
+}
