9af21d3c2d5d5a855fbb75638cd09f6bbb28beb5
Thomas Bärwinkel <tbl@gmx.de>
2020-09-18 13:57:07 +0200

    change version in docker

--- src/main/docker/build-docker.sh
+++ src/main/docker/build-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-cp ../../../target/solr-cmd-utils-3.11-SNAPSHOT-cmd.tar.gz .
+cp ../../../target/solr-cmd-utils-3.13-SNAPSHOT-cmd.tar.gz .
 sudo docker build -t solr-cmd-utils .
 sudo docker tag solr-cmd-utils tblsoft/solr-cmd-utils
 sudo docker push tblsoft/solr-cmd-utils:latest
