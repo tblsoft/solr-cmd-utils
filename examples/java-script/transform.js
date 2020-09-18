72d94357727a730047faa195db06d5c94224c31a
Marius Kolkowski <marius.kolkowski@raystorm.place>
2020-09-18 14:07:55 +0200

    revert change

--- examples/java-script/transform.js
+++ examples/java-script/transform.js
@@ -1,23 +1,9 @@
-// ParagraphAttributeExtractor
-// set params
-var html =  "<html>\n" +
-            "<body>\n" +
-            "    <div class=\"product-info-wrapper\">\n" +
-            "        <p><strong>Decksohle:</strong><br>Leder</p>\n" +
-            "        <p><strong>Form:</strong><br>Sandale/Pantolette</p>\n" +
-            "        <p><strong>Futter:</strong><br>Leder</p>\n" +
-            "        <p><strong>Farbe:</strong><br>Grau</p>\n" +
-            "    </div>\n" +
-            "</body>\n" +
-            "</html>";
-var jsoupDoc = htmlParser.parseInput(html, "");
-var selector = ".product-info-wrapper > p";
-
-// parse
-var elements = jsoupDoc.select(selector);
-elements.forEach(function(element) {
-    var splitted = element.text().split(":");
-    var key = splitted[0];
-    var value = splitted[1];
-    print("attr_"+key+": "+value);
-});
+document = documentBuilder.create();
+document.addField("foo", "bar");
+document.addField("tbl", "bar");
+document.addField("tbl", "alice");
+document.addField("tbl", "bob");
+document.setField("alice", input.getField("column1").getValues());
+document.setField("alice", input.getField("alice").getValues());
+output.add(document);
+output.add(input);
