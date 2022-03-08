9e9ed9db829fa6535f81d78244f9b536185034b5
Marius Kolkowski <marius.kolkowski@raystorm.place>
2022-03-08 15:43:51 +0100

    fix nullptr

--- src/main/java/de/tblsoft/solr/util/DocumentMapper.java
+++ src/main/java/de/tblsoft/solr/util/DocumentMapper.java
@@ -16,7 +16,7 @@ public class DocumentMapper {
 	public static Map<String, Object> toMap(Document document) {
 		Map<String, Object> outputDocument = new HashMap<>();
 		for(Field field : document.getFields()) {
-			if(field.getValues().size() == 1) {
+			if(field.getValues() != null && field.getValues().size() == 1) {
 				outputDocument.put(field.getName(), field.getValue());
 			} else {
 				outputDocument.put(field.getName(), field.getValues());
