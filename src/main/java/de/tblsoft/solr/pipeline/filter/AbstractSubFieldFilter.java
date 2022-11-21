6504e54437fcd9662bf7f9596adb25eae2e155e7
Marius Kolkowski <marius.kolkowski@quasiris.de>
2022-11-21 14:52:56 +0100

    add auto create subField from rawValue

--- src/main/java/de/tblsoft/solr/pipeline/filter/AbstractSubFieldFilter.java
+++ src/main/java/de/tblsoft/solr/pipeline/filter/AbstractSubFieldFilter.java
@@ -6,6 +6,7 @@ import de.tblsoft.solr.pipeline.bean.Document;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public abstract class AbstractSubFieldFilter extends AbstractFilter {
@@ -20,9 +21,25 @@ public abstract class AbstractSubFieldFilter extends AbstractFilter {
 	}
 
 	public List<Document> traverse(int i, String[] fieldNames, Document document) {
-		List<Document> documents = document.getSubField(fieldNames[i]);
+		String fieldName = fieldNames[i];
+		List<Document> documents = document.getSubField(fieldName);
 		if(documents == null) {
-			return Arrays.asList(document);
+			// retrieve subfield from rawvalue field
+			if(document.getField(fieldName).getRawValue() != null) {
+				List<Map<String, Object>> rawDocs = (List<Map<String, Object>>) document.getField(fieldName).getRawValue();
+				document.deleteField(fieldName);
+
+				for (Map<String, Object> rawDoc : rawDocs) {
+					Document subDoc = new Document();
+					for (Map.Entry<String, Object> entry : rawDoc.entrySet()) {
+						subDoc.setField(entry.getKey(), entry.getValue());
+						document.addSubField(fieldName, subDoc);
+					}
+				}
+				documents = document.getSubField(fieldName);
+			} else {
+				return Arrays.asList(document);
+			}
 		}
 		List<Document> processedDocumentList = new ArrayList<>();
 		for(Document d : documents) {
